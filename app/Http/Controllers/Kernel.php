fbc59e8feb01a764afc08c27a3097ab3d340276e
hamlulkhair <khairhamlul@gmail.com>
2025-07-14 13:09:31 +0700

    Update Kernel.php

--- app/Http/Controllers/Kernel.php
+++ app/Http/Controllers/Kernel.php
@@ -40,9 +40,9 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Http\Middleware\HandleCors::class,  // Pastikan ini ada di sini
+            \Illuminate\Http\Middleware\HandleCors::class,
         ],
 
     ];
